309441d7ecc98dcfeb902571b37fb024638784ce
Jesus <jperrob244@fp.iesromerovargas.com>
2025-05-28 01:49:20 +0200

    Admin al populate.

--- poputale_db.py
+++ poputale_db.py
@@ -17,6 +17,11 @@ from PFinance.models import (
 
 def create_users():
     # Crear usuarios
+    admin = User.objects.create_superuser(
+        username='admin',
+        email='admin@admin.com',
+        password='admin'
+    )
     user1 = User.objects.create_user(
         username='juan',
         email='juan@example.com',
@@ -27,6 +32,7 @@ def create_users():
 
     # Crear perfiles de usuario
     UserProfile.objects.create(user=user1, currency='EUR', notification_app=True)
+    UserProfile.objects.create(user=admin, currency='EUR', notification_app=True)
 
     return user1
 
